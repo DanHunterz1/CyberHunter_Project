634afc23342c2f142ed8e7008c3a483b39308f2b
Danrley <Danrleygja@gmail.com>
2024-06-09 22:11:30 -0300

    17ª tentativa

---
+++ src/main/java/com/CyberHunter/CyberHunter/CyberHunterApplication.java
@@ -0,0 +1,13 @@
+package com.CyberHunter.CyberHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberHunterApplication.class, args);
+	}
+
+}
